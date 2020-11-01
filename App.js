e7ff1313de7d4b715f4392692376cb8e7420c903
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-11-01 21:46:09 +0530

    squares

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Starting an Awesome Project with React Native.</Text>
+      <View style={styles.small}>
+        <Text>Small</Text>
+      </View>
+      <View style={styles.medium}>
+        <Text>Medium</Text>
+      </View>
+      <View style={styles.large}>
+        <Text>Large</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +27,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  small: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'powderblue'
+  },
+  medium: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'skyblue'
+  },
+  large: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'steelblue'
+  }
 });
